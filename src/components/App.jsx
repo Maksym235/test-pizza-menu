cb96694a9adb42b3ec35f1f9cfd57223e8b1849c
Maksym235 <andriichukwork235@gmail.com>
2023-03-05 14:28:15 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
 
 import data from 'Data/data.json';
 import { Section } from './Section/Section';
 import { PizzaList } from './PizzaList/PizzaList';
 import { Modal } from './Modal/Modal';
 import { FormikForm } from './Form/Form';
-export class App extends React.Component {
-  state = {
-    pizzas: data,
-    isOpen: false,
-  };
 
-  togleBtn = () => {
-    this.setState(({ isOpen }) => ({
-      isOpen: !isOpen,
-    }));
-  };
+export function App() {
+  const [pizzas, setPizzas] = useState(data);
+  const [isOpen, setOpen] = useState(false);
 
-  render() {
-    return (
-      <Section title={`Pizza menu`}>
-        {this.state.isOpen && (
-          <Modal onClose={this.togleBtn}>
-            <FormikForm />
-          </Modal>
-        )}
-        <PizzaList pizzas={this.state.pizzas} modalBtn={this.togleBtn} />
-      </Section>
-    );
+  function togleBtn(evt) {
+    setOpen(!isOpen);
   }
+
+  return (
+    <Section title={`Pizza menu`}>
+      {isOpen && (
+        <Modal onClose={togleBtn}>
+          <FormikForm />
+        </Modal>
+      )}
+      <PizzaList pizzas={pizzas} modalBtn={togleBtn} />
+    </Section>
+  );
 }
+
+// export class App extends React.Component {
+//   state = {
+//     pizzas: data,
+//     isOpen: false,
+//   };
+
+//   togleBtn = () => {
+//     this.setState(({ isOpen }) => ({
+//       isOpen: !isOpen,
+//     }));
+//   };
+
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
